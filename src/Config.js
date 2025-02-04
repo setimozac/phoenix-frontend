5b335f9e76223f65abe088f2337234382bb1e12b
moe <sizdah@gmail.com>
2025-02-03 20:44:22 -0500

    Added default value in Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,12 @@
 const getConfig = async () => {
-  const response = await fetch('/config.json');
+  try {
+    const response = await fetch('/config.json');
+  if (!response.ok) {return {BACKEND_URL: "http://phoenix:8080"}}
   return response.json();
+  } catch (error) {
+    return {BACKEND_URL: "http://phoenix:8080"}
+  }
+  
 };
 
 export default getConfig
